b04759f6f4604b28dd737774414e3d3b3c9cfb63
James Parker <jamez226@hotmail.com>
2014-10-11 09:53:53 +0100

    Initial Commit

---
+++ HelloWorld.Tests/HelloWorldTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace HelloWorld.Tests
+{
+    [TestFixture]
+    public class HelloWorldTests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(true, Is.EqualTo(true));
+        }
+    }
+}
